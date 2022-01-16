a3ff2e2eaed2218f1400b1aec0beeab8e60f8608
BajrangBLB <leb081118@gmail.com>
2022-01-16 10:00:58 +0530

    Commit when deploying the project

--- Hello/settings.py
+++ Hello/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^j04txxpl4dz36wy@wkvrvcs%vf1-j)-aj)u#8psvgy!2_!zk)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://yt-phone.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
