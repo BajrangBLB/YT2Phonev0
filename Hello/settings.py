6f550e97b016b02b58be9727c3b96f76960529c9
BajrangBLB <83524457+BajrangBLB@users.noreply.github.com>
2022-03-02 16:45:57 +0530

    Changed Allowed Hosts

--- Hello/settings.py
+++ Hello/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^j04txxpl4dz36wy@wkvrvcs%vf1-j)-aj)u#8psvgy!2_!zk)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['yt-phone.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'www.yt2phone.com', 'yt2phone.com', '137.184.226.22']
 
 
 # Application definition
